f2f271b2d4d37728701fe387a26b1f6e9de92dcf
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 15:38:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,9 @@
 class Song
+  attr_accessor :title
+
+def initialize(title)
+  @title = title
+end
 #needs to belong to an artist
 #Songs should have a title.
 # A song should be able to tell you the name of its artist:
