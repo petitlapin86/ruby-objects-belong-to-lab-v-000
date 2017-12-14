c460e23e0b665d85153b1a87500e27232bc94b8e
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 15:37:20 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,5 @@
 class Post
 #post should belong to an author
+#Posts should have a title 
+# A post should be able to tell you the name of its author:
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-#needs to belong to an artist 
+#needs to belong to an artist
 #Songs should have a title.
 # A song should be able to tell you the name of its artist:
-end 
+end
